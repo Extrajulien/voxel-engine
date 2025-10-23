ec87d055ae7b54ec5237a3fbaed7da79d012dfc3
Extrajulien <julien.lamothe01@gmail.com>
2025-10-23 17:32:33 -0400

    refactor: simplify shader compilation

--- src/main/java/doctrina/rendering/Shader.java
+++ src/main/java/doctrina/rendering/Shader.java
@@ -1,5 +1,6 @@
 package doctrina.rendering;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -10,31 +11,15 @@ public class Shader {
 
     public Shader(String vertexShaderFile, String fragmentShaderFile) {
         try {
-            String vertexCode = Files.readString(Paths.get(vertexShaderFile));
-            int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-            glShaderSource(vertexShader, vertexCode);
-            glCompileShader(vertexShader);
-            checkShaderCompileErrors(vertexShader, "Vertex");
-
-
-            String fragmentCode = Files.readString(Paths.get(fragmentShaderFile));
-            int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragmentShader, fragmentCode);
-            glCompileShader(fragmentShader);
-            checkShaderCompileErrors(fragmentShader, "Fragment");
-
-
             shaderProgramId = glCreateProgram();
-            glAttachShader(shaderProgramId, vertexShader);
-            glAttachShader(shaderProgramId, fragmentShader);
-            glLinkProgram(shaderProgramId);
-            checkShaderLinkingErrors();
+            int vertexShader =  compileShader(vertexShaderFile, GL_VERTEX_SHADER);
+            int fragmentShader = compileShader(fragmentShaderFile, GL_FRAGMENT_SHADER);
+            linkShadersToProgram();
 
             glDeleteShader(vertexShader);
             glDeleteShader(fragmentShader);
         } catch (Exception e) {
-            System.err.println("ERROR shader could no be read");
-            System.exit(1);
+            stopProgram();
         }
     }
 
@@ -43,17 +28,52 @@ public class Shader {
     }
 
     public void setUniform(String uniform, float value) {
+        glUniform1f(glGetUniformLocation(shaderProgramId, uniform), value);
+    }
 
+    private int compileShader(String shaderFile, int shaderType) throws IOException {
+        String fragmentCode = Files.readString(Paths.get(shaderFile));
+        int shader = glCreateShader(shaderType);
+        glShaderSource(shader, fragmentCode);
+        glCompileShader(shader);
+        checkShaderCompileErrors(shader, shaderType);
+        glAttachShader(shaderProgramId, shader);
+        return shader;
     }
 
 
-    private void checkShaderCompileErrors(int shaderId, String Label) {
+    private void checkShaderCompileErrors(int shaderId, int shaderType) {
         int success = glGetShaderi(shaderId, GL_COMPILE_STATUS);
         if (success == GL_FALSE) {
             String infoLog = glGetShaderInfoLog(shaderId);
-            System.err.println("ERROR shader compilation error of type: " + Label + "\n" + infoLog);
+            System.err.println("ERROR shader compilation error of type: " +
+                    getShaderNameFromShaderType(shaderType) + "\n" + infoLog);
             System.exit(1);
         }
+    }
+
+    private String getShaderNameFromShaderType(int shaderType) {
+        switch (shaderType) {
+            case GL_FRAGMENT_SHADER -> {
+                return "Fragment";
+            }
+            case GL_VERTEX_SHADER -> {
+                return "Vertex";
+            }
+            default -> {
+                return "Unknown";
+            }
+        }
+    }
+
+    private void linkShadersToProgram() {
+        glLinkProgram(shaderProgramId);
+        checkShaderLinkingErrors();
+    }
+
+    private void stopProgram() {
+        System.err.println("ERROR shader could no be read");
+        System.exit(1);
     }
 
     private void checkShaderLinkingErrors() {
