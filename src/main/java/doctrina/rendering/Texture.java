cc0e07c6dd89d576df268cad767833b7b9649c8f
Extrajulien <julien.lamothe01@gmail.com>
2025-10-27 13:53:52 -0400

    feat: Create Texture class

---
+++ src/main/java/doctrina/rendering/Texture.java
@@ -0,0 +1,46 @@
+package doctrina.rendering;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.stb.STBImage;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL11C.*;
+import static org.lwjgl.opengl.GL30C.glGenerateMipmap;
+import static org.lwjgl.stb.STBImage.*;
+
+public class Texture {
+    private final int textureID;
+
+    public Texture(String fileName) {
+        ByteBuffer imageData;
+        IntBuffer width = BufferUtils.createIntBuffer(1);
+        IntBuffer height = BufferUtils.createIntBuffer(1);
+        IntBuffer nbChannels = BufferUtils.createIntBuffer(1);
+        imageData = stbi_load(fileName, width, height, nbChannels, 0);
+
+        textureID = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, textureID);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        if (imageData == null) {
+            System.err.println("ERROR Failed to load image: " + STBImage.stbi_failure_reason());
+            System.exit(1);
+        }
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width.get(0), height.get(0),
+                0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        stbi_image_free(imageData);
+    }
+
+    public int getTextureID() {
+        return textureID;
+    }
+}
