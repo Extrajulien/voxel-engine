40d4b09f1422a824d51bc106237ff50831a52dfd
Extrajulien <julien.lamothe01@gmail.com>
2025-11-23 13:43:58 -0500

    feat: modify test entity class

--- src/main/java/demo_game/EntityTest.java
+++ src/main/java/demo_game/EntityTest.java
@@ -1,12 +1,16 @@
 package demo_game;
 
 import doctrina.Entities.Entity;
+import doctrina.debug.Color;
+import doctrina.physic.HitBox;
 import doctrina.rendering.Model;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3d;
+import org.joml.Vector3f;
 
 public class EntityTest extends Entity {
-
     public EntityTest(Model model) {
-        super(model);
+        super(model, new Vector3f(1,1,1));
+        hitbox.setColor(Color.YELLOW);
     }
 }
